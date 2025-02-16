6ba9082957261f87f5a7b8be6e564366b83122fe
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2025-02-16 10:02:40 +0900

    Remove CaretLayer

--- src/AvaloniaHex/HexEditor.cs
+++ src/AvaloniaHex/HexEditor.cs
@@ -49,7 +49,6 @@ public class HexEditor : TemplatedControl
 
         HexView.Layers.InsertBefore<TextLayer>(new CurrentLineLayer(Caret, Selection));
         HexView.Layers.InsertBefore<TextLayer>(new SelectionLayer(Caret, Selection));
-        HexView.Layers.Add(new CaretLayer(Caret));
         HexView.DocumentChanged += HexViewOnDocumentChanged;
 
         Caret.PrimaryColumnIndex = 1;
